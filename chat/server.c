b9056b013db0a32ba3e604e2cf33d5eb7e9902e4
michailivanov <85007060+michailivanov@users.noreply.github.com>
2023-12-19 18:38:59 +0300

    Update server.c

--- chat/server.c
+++ chat/server.c
@@ -15,16 +15,9 @@
 static unsigned char ENC_AES_KEY[] = {0, 10, 25, 43, 4, 135, 246, 78, 118, 29, 101, 156, 126, 16, 15, 1};
 #endif
 
-static int CHILD_SOCK = -1;                             // This variable only used in child process to indicate socket
+static int CHILD_SOCK = -1; // This variable only used in child process to indicate socket
 static pid_t CHILDREN_PROCESS[MAX_CHILD_PROCESS] = {0}; // This variable only used in parent process to record all children processes
 
-/* Try to collect subprocess pid into CHILDREN_PROCESS array.
-It will traverse CHILDREN_PROCESS pid array until get a free space for new process.
-
-Returns:
-    pid_index when set it successfully
-    -1        when not more position for new process
-*/
 int toggle_child_process_flag(pid_t from, pid_t to)
 {
     for (int index = 0; index < MAX_CHILD_PROCESS; ++index)
@@ -38,10 +31,6 @@ int toggle_child_process_flag(pid_t from, pid_t to)
     return -1;
 }
 
-/* Capture SIGTERM signal for parent process and kill all children precesses.
-
-It will also set process pid in given position to 0.
-*/
 void handle_exit_in_parent()
 {
     for (int index = 0; index < MAX_CHILD_PROCESS; ++index)
@@ -59,7 +48,6 @@ void handle_exit_in_parent()
     exit(0);
 }
 
-/* Capture SIGCHILD signal and remove pid of terminated child process from CHILDREN_PROCESS. */
 void handle_defunct()
 {
     pid_t pid;
@@ -70,13 +58,11 @@ void handle_defunct()
     }
 }
 
-/* Capture SIGTERM in child process for closing socket.  */
 void handle_sigterm_in_child()
 {
     close(CHILD_SOCK);
 }
 
-/* Reverse a given string */
 void reverse_string(char *string, int length)
 {
     int start = 0, end = length - 1;
@@ -90,13 +76,38 @@ void reverse_string(char *string, int length)
     }
 }
 
+// New function to handle outgoing messages from server to client
+void send_to_client(int client, struct sockaddr_in *addr)
+{
+    char data[65535];
+
+    while (1)
+    {
+        printf(">> ");
+        fgets(data, 1024, stdin);
+        unsigned short size = strlen(data);
+
+#ifdef ENABLE_CRYPTO
+
+        // Encrypt data
+        unsigned char encrypted[65535] = {0};
+        int length = encrypt_aes128((const unsigned char *)data, size, ENC_AES_KEY, encrypted);
+        isend(client, encrypted, length);
+
+#else
+
+        isend(client, data, size);
+
+#endif
+    }
+}
+
 void serve_client(int client, struct sockaddr_in *addr)
 {
     char data[65535];
 
     while (1)
     {
-        // Receive data from client
         int received = ireceive(client, data);
         if (received < 0)
             return;
@@ -107,17 +118,14 @@ void serve_client(int client, struct sockaddr_in *addr)
 
 #ifdef ENABLE_CRYPTO
 
-        // Try to decrypt data, if failed, do not reply and close connection
         char decrypted[65535];
         int length = decrypt_aes128((const unsigned char *)data, received, ENC_AES_KEY, (unsigned char *)decrypted);
         if (length < 0)
             return;
         printf("DEBUG: decrypted message %s", decrypted);
 
-        // Reverse string
         reverse_string(decrypted, length - 1);
 
-        // Encrypted again
         memset(data, 0, received);
         int send = encrypt_aes128((const unsigned char *)decrypted, length, ENC_AES_KEY, (unsigned char *)data);
         printf("DEBUG: encrypted message with %i size\n", send);
@@ -129,7 +137,6 @@ void serve_client(int client, struct sockaddr_in *addr)
 
 #endif
 
-        // Try to decrypt data and echo it back
         if (isend(client, data, send) < 0)
             return;
         printf("INFO: sent %i bytes to %s:%i\n",
@@ -137,29 +144,22 @@ void serve_client(int client, struct sockaddr_in *addr)
                inet_ntoa(addr->sin_addr),
                addr->sin_port);
 
-        // Reset data buffer
         memset(data, 0, send);
     }
 }
 
 int main(int argc, const char *argv[])
 {
-    // Capture SIGINT, SIGSTP signal and terminate all children processes
     signal(SIGINT, handle_exit_in_parent);
-
-    // Capture SIGCHLD signal when child process quited
     signal(SIGCHLD, handle_defunct);
 
-    // Check if argc == 3, otherwise exit
     if (argc != 3)
         return -1;
 
-    // Create listener socket for accepting new requests from client
     int server = iconnect(argv[1], atoi(argv[2]), bind);
     if (server < 0 || listen(server, SOMAXCONN) < 0)
         return -2;
 
-    // Try to accept and serve client
     while (1)
     {
         struct sockaddr_in client_addr;
@@ -174,18 +174,30 @@ int main(int argc, const char *argv[])
             return -2;
         }
 
-        // Fork a new process to serve client and close reference of listener socket
         if (pid == 0)
         {
             close(server);
             CHILD_SOCK = client;
             signal(SIGTERM, handle_sigterm_in_child);
+
+            // Create a new process for handling outgoing messages from server to client
+            int sender_pid = fork();
+            if (sender_pid < 0)
+            {
+                perror("ERROR: fork sender process failed\n");
+                exit(0);
+            }
+            if (sender_pid == 0)
+            {
+                send_to_client(client, &client_addr);
+                exit(0);
+            }
+
+            // Serve the client in the current process
             serve_client(client, &client_addr);
             exit(0);
         }
 
-        // Collect child process pid to an array and close reference of client socket
-        // If client not collected by father process, tell child process to terminate self
         if (pid > 0)
         {
             printf("DEBUG: subprocess %i has started\n", pid);
